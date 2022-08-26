718830461a48ff4925e90a4ef43cf79c4e8f3489
yellhtetaung <yairhtet248.yha@gamil.com>
2022-08-26 15:37:57 +0630

    26/8/22

--- vegoniauniversity/js/app.js
+++ vegoniauniversity/js/app.js
@@ -1,3 +1,45 @@
+// Start Jquery Area
+
 $(document).ready(function () {
-  console.log("hi");
+  // Start Header
+  // Start Nav Bar
+  $(".navbuttons").click(function () {
+    $(this).toggleClass("crossxs");
+  });
+  // End Nav Bar
+  // End Header
 });
+
+// End Jquery Area
+
+// Start Javascript Area
+// Start Counter Section
+var getcountervalues = document.querySelectorAll(".countervalues");
+getcountervalues.forEach(function (getcountervalue) {
+  // console.log(getcountervalue);
+
+  getcountervalue.textContent = "0";
+
+  const updatecounter = function () {
+    // console.log("i am working");
+
+    const getcttarget = +getcountervalue.getAttribute("data-target");
+    // console.log(getcttarget);
+    // console.log(typeof getcttarget, getcttarget);
+
+    const getctcontent = +getcountervalue.innerText;
+    // console.log(typeof getctcontent);
+
+    const incnumber = getcttarget / 100;
+    // console.log(incnumber);
+
+    if (getctcontent < getcttarget) {
+      getcountervalue.textContent = getctcontent + incnumber;
+      setTimeout(updatecounter, 50);
+    }
+  };
+
+  updatecounter();
+});
+// End Counter Section
+// End Javascript Area
